8bb949367d18b0db88a8da938f80cc8726d29c06
Zhukov531 <zhukov531@gmail.com>
2022-06-18 13:02:49 +0300

    footer complete

---
+++ send.php
@@ -0,0 +1,11 @@
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+echo $fio;
+echo "<br>";
+echo $email;
